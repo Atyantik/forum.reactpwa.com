a3e480fd7040bdc411c5dc07738ec706fbc1a14b
Tirth Bodawala <tirthbodawala@atyantik.com>
2019-02-08 13:23:24 +0530

    Initializing flarum setup for forum

---
+++ sample.config.php
@@ -0,0 +1,21 @@
+<?php
+return array (
+  'debug' => false,
+  'database' =>  array (
+    'driver' => 'mysql',
+    'host' => '<host>',
+    'database' => '<database name>',
+    'username' => '<database user>',
+    'password' => '<database password>',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => 'flm_',
+    'port' => '3306',
+    'strict' => false,
+  ),
+  'url' => '<url running the code>',
+  'paths' =>  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
